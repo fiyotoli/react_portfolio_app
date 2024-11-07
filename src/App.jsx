c671487e5fd1bfdb2e3cd155b55eab58823b5f42
fiyotoli <tasfayneshtolasa35@gmail.com>
2024-11-07 12:44:35 +0300

    update deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import Footer from './components/Footer/Footer';
 
-
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-            
-            </Routes>
-            <Footer/>
-        </Router>
-    );
+  return (
+    <Router basename="/react_portfolio_app">  {/* Replace '/react_portfolio_app' with your actual repo name */}
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Add other routes here as needed */}
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
