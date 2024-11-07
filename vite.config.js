747a0d2ef6eee68100c66172568266e1315076f1
fiyotoli <tasfayneshtolasa35@gmail.com>
2024-11-07 12:33:25 +0300

    deploy app

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react_portfolio_app/',// Replace 'your-repo-name' with the name of your GitHub repository
+
 })
